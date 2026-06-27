abc7153046d40a3c55ee7e1d18c4ef9bc6225784
Annie Priyanka <anniepriyanka03@gmail.com>
2026-06-27 14:28:30 +0530

    Added GitHub push example

--- practice.py
+++ practice.py
@@ -1,9 +1,7 @@
 print("Heyy Annie")
 print("I am learning Git")
 print("Another Change")
-<<<<<<< HEAD
 print("Hello from main branch")
-=======
 print("Hello from git conflict branch")
->>>>>>> conflict-demo
 print("Caluclator Feature")
+print("Learning GitHub Page")
